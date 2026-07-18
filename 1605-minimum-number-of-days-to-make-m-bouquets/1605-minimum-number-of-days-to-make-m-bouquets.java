68d0b2b4219f6ec5ac1b90234de5e4199b69d2f4
Aryan Dave <115967943+aryagnik@users.noreply.github.com>
2026-07-18 18:39:19 +0530

    Time: 24 ms (10.3%), Space: 82.7 MB (48.42%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,17 +1,19 @@
 class Solution {
 
-    public static int findMax(int[] arr){
+    public static int[] findMax(int[] arr){
         int max = arr[0];
+        int min = arr[0];
 
         for(int i=1; i<arr.length; i++){
             if(max<arr[i])max = arr[i];
+            if(min>arr[i])min = arr[i];
         }
 
-        return max;
+        return new int[] {min,max};
     }
 
-    public static int binary_search(int[] arr, int m, int k, int max){
-        int low = 1;
+    public static int binary_search(int[] arr, int m, int k, int min, int max){
+        int low = min;
         int high = max;
 
         int ans = -1;
@@ -54,9 +56,12 @@ class Solution {
             return -1;
         }
 
-        int max = findMax(bloomDay);
+        int[] arr1 = findMax(bloomDay);
 
-        int ans = binary_search(bloomDay,m,k,max);
+        int min = arr1[0];
+        int max = arr1[1];
+
+        int ans = binary_search(bloomDay,m,k,min,max);
 
         return ans;
     }
